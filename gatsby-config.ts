3c63d48da22605dcc0b9ef0c1ae1f41d6a1ef381
natainditama <natainditama@gmail.com>
2022-08-15 09:09:57 +0800

    refactor: Replace postcss to styled-components

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
