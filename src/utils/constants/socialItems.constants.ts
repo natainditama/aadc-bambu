5adb693921b1bd7c36c43176a221bb43658b608a
natainditama <natainditama@gmail.com>
2022-08-16 18:23:14 +0800

    utils: Add list social media

---
+++ src/utils/constants/socialItems.constants.ts
@@ -0,0 +1,25 @@
+interface SocialItems {
+  name?: string;
+  icon?: string;
+  url?: string;
+}
+
+const socialItems: Array<SocialItems> = [
+  {
+    name: "instagram",
+    icon: "instagram.png",
+    url: "https://www.instagram.com/ayunibambu0404/",
+  },
+  {
+    name: "facebook",
+    icon: "facebook.png",
+    url: "https://www.facebook.com/igusti.murniati.1",
+  },
+  {
+    name: "tiktok",
+    icon: "tiktok.png",
+    url: "https://www.tiktok.com/@aadcbambu",
+  },
+];
+
+export default socialItems;
