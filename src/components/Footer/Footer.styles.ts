925e148ccbe7df4b265d6631e39827c5ed63eb9a
natainditama <natainditama@gmail.com>
2022-08-17 12:35:19 +0800

    style: Implement footer with container

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -2,21 +2,16 @@ import styled from "styled-components";
 
 const FooterBar = styled.footer`
   background-color: #1c1f2a;
-  padding: 1.2rem 2rem;
-  display: flex;
-  justify-content: space-between;
-  flex-direction: column-reverse;
-  align-items: center;
+  padding: 1.2rem 0;
   & > div {
-    margin-bottom: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column-reverse;
   }
-  @media only screen and (min-width: 1000px) {
-    & {
-      padding-left: 120px;
-      flex-direction: row;
-    }
+  @media screen and (min-width: 1024px) {
     & > div {
-      margin-bottom: 0;
+      flex-direction: row;
     }
   }
 `;
@@ -25,6 +20,14 @@ const FooterCopyright = styled.p`
   font-size: 1rem;
   line-height: 1.5rem;
   color: #f7f7f6;
+  & {
+    margin-top: 5px;
+  }
+  @media screen and (min-width: 1024px) {
+    & {
+      margin-top: 0;
+    }
+  }
 `;
 
 const FooterSocial = styled.div`
@@ -34,11 +37,6 @@ const FooterSocial = styled.div`
   justify-content: center;
   & * + * {
     margin-left: 10px;
-  }
-  @media only screen and (min-width: 1000px) {
-    & > .logo {
-      transform: scale(1.5);
-    }
   }
 `;
 
