de8e252bb7e78519ecdcd57c655986d288f35b3c
natainditama <natainditama@gmail.com>
2022-08-17 12:34:46 +0800

    chore: Remove social media on mobile

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
 import { FooterBar, FooterSocial, FooterCopyright } from "./Footer.styles";
 import socialItems from "../../utils/constants/socialItems.constants";
 import { useStaticQuery, graphql } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
+import Container from "../Container/Container";
 
 const Footer = () => {
-  const [isMobile, setIsMobile] = useState(true);
-
   const data = useStaticQuery(graphql`
     {
       allFile(
@@ -27,28 +24,12 @@ const Footer = () => {
     }
   `);
 
-  useEffect(function () {
-    let handleResize: () => void;
-    if (typeof window !== "undefined") {
-      handleResize = () => setIsMobile(window.innerWidth < 768);
-      handleResize();
-      window.addEventListener("DOMContentLoaded", handleResize);
-      window.addEventListener("resize", handleResize);
-    }
-    return function () {
-      if (typeof window !== "undefined") {
-        window.removeEventListener("resize", handleResize);
-        window.removeEventListener("DOMContentLoaded", handleResize);
-      }
-    };
-  }, []);
-
   return (
     <FooterBar>
-      <FooterCopyright>Copyright © 2022. All rights reserved.</FooterCopyright>
-      <FooterSocial>
-        {isMobile &&
-          data.allFile.edges.map(({ node: { name, childImageSharp } }: any, index: number) => {
+      <Container>
+        <FooterCopyright>Copyright © 2022. All rights reserved.</FooterCopyright>
+        <FooterSocial>
+          {data.allFile.edges.map(({ node: { name, childImageSharp } }: any, index: number) => {
             return (
               <a
                 href={socialItems[index].url}
@@ -64,15 +45,8 @@ const Footer = () => {
               </a>
             );
           })}
-        <Link to="/" className="logo">
-          <StaticImage
-            src="https://yt3.ggpht.com/ytc/AMLnZu_e5SkKL2szVns3FcibCCrvIG7gFMH6HTat7-oy=s88-c-k-c0x00ffffff-no-rj"
-            style={{ width: "25px", height: "25px", borderRadius: "50%" }}
-            placeholder="tracedSVG"
-            alt="AADC Bambu"
-          />
-        </Link>
-      </FooterSocial>
+        </FooterSocial>
+      </Container>
     </FooterBar>
   );
 };
