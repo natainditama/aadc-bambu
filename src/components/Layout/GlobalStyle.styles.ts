dff5648cd9b8aee9e0abe14f5ab5cd66f48d2b19
natainditama <natainditama@gmail.com>
2022-09-18 07:53:13 +0800

    styles: Update style tsParticles instance

--- src/components/Layout/GlobalStyle.styles.ts
+++ src/components/Layout/GlobalStyle.styles.ts
@@ -293,6 +293,16 @@ const GlobalStyle = createGlobalStyle`
   [hidden] {
     display: none;
   }
+
+  .bg-particles{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+    display: block;
+  }
 `;
 
 export default GlobalStyle;
