00ad94267fb41ac96d9dbefdb2b81361530d94d4
natainditama <natainditama@gmail.com>
2023-01-03 12:20:55 +0800

    chore: Setup path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import alias from "@rollup/plugin-alias";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    alias({
+      entries: [
+        {
+          find: "@",
+          replacement: resolve(__dirname, "src"),
+        },
+      ],
+    }),
+  ],
+});
