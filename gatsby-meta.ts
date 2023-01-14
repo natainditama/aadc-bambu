b1dd2e7df8cfc09f6dbe90acb276e680dda06f70
natainditama <natainditama@gmail.com>
2023-01-14 23:23:02 +0800

    feat: Create gatsby SEO config

---
+++ gatsby-meta.ts
@@ -0,0 +1,16 @@
+const metaConfig = {
+  title: "AADC Bambu",
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis sed consequuntur exercitationem dolorem accusantium? Repudiandae, deserunt`,
+  author: "natainditama",
+  siteUrl: "https://aadc-bambu.web.app/",
+  lang: "id",
+  links: {
+    github: "https://github.com/natainditama/aadc-bambu",
+    instagram: "https://www.instagram.com/ayunibambu0404/",
+    tiktok: "https://www.tiktok.com/@aadcbambu",
+    facebook: "https://www.facebook.com/igusti.murniati.1",
+  },
+  favicon: "src/images/logo.jpg",
+};
+
+export default metaConfig
