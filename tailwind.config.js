3d33258b09df26c440394ea95b7f2f6f2f9d57fc
natainditama <natainditama@gmail.com>
2023-01-14 23:27:55 +0800

    style: Update default theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,28 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = withMT({
-  content: [
-    "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: colors.neutral[50],
+        dark: colors.neutral[800],
+        primary: colors.green[600],
+        ...colors,
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+      },
+    },
+    fontFamily: {
+      sans: ["Lexend", ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
 });
