e19dd46707c35440a4881c4524f522f459087af6
natainditama <natainditama@gmail.com>
2023-01-15 19:50:43 +0800

    feat: Update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,52 @@
-import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
-import "../styles/global.css";
+import React from "react";
+import type { PageProps } from "gatsby";
+import { VideoItem, SEO } from "@/components";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { Video } from "@/types/videos";
 
-const IndexPage: React.FC<PageProps> = () => {
+const CHANNEL_ID = "UCVVQiuzOR_MlSOax9KPnuIA";
+
+const HomePage: React.FC<PageProps> = () => {
+  const { status, data, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async (): Promise<{ items: Array<Video> }> => {
+      const { data } = await axios.get(
+        `${process.env.YOUTUBE_API_URL}search?part=snippet&part=id&channelId=${CHANNEL_ID}&order=viewCount&maxResults=12&key=${process.env.YOUTUBE_API_KEY}`
+      );
+      return data;
+    },
+  });
+
   return (
-    <main>
-      <div className="header-logo"></div>
-      <article className="title-section">
-        <h1>
-          We are launching <span className="soon">soon!</span>
-        </h1>
-        <p className="title-info">Subscribe and get notified</p>
-      </article>
-      <article className="input-section">
-        <a href="https://www.youtube.com/@aadcbambu" target="_blank" rel="noopener noreferrer">
-          <svg
-            role="img"
-            viewBox="0 0 24 24"
-            style={{ fill: "#FF0000", height: "5rem" }}
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>YouTube</title>
-            <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
-          </svg>
-        </a>
-      </article>
-      <article>
-        <div className="statistics-img">
-          <img
-            src="https://catherineisonline.github.io/ping-coming-soon-page-frontendmentor/images/illustration-dashboard.png"
-            alt="Statistics Image"
-          />
+    <>
+      <SEO title="Home Page" />
+      <section className="videos-wrap">
+        <div className="container mx-auto">
+          {status === "loading" ? (
+            "Loading..."
+          ) : error instanceof Error ? (
+            <span>Error: {error.message}</span>
+          ) : (
+            <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-6">
+              {data?.items.map((item) => {
+                return (
+                  <VideoItem
+                    key={item.id.videoId}
+                    title={item.snippet.title}
+                    publishedAt={item.snippet.publishedAt}
+                    image={
+                      <img src={item.snippet.thumbnails.medium.url} alt={item.snippet.title} />
+                    }
+                  />
+                );
+              })}
+            </div>
+          )}
         </div>
-      </article>
-    </main>
+      </section>
+    </>
   );
 };
 
-export default IndexPage;
-
-export const Head: HeadFC = () => <title>Home Page</title>;
+export default HomePage;
