c2763b0af2b84e7ccfd7875c28f338d271ea8fbb
natainditama <natainditama@gmail.com>
2023-01-15 19:48:08 +0800

    refactor: Update Footer type

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import type { Socials } from "@utils/constants/socials";
 import { Typography, IconButton } from "@material-tailwind/react";
 import { useStaticQuery, graphql } from "gatsby";
 import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image";
+import { Socials } from "@/utils/constants/socials";
 
 export type FooterProps = {
-  title: string;
-  description: string ;
+  title: Queries.Maybe<string>;
+  description: Queries.Maybe<string>;
   socials: Socials[];
-  menus: { name: string; items: { name: string; path: string }[] }[];
 };
 
 export type FooterData = {
   allFile: Queries.FileConnection;
 };
 
-const Footer: React.FC<FooterProps> = ({ title, description, socials, menus }: FooterProps) => {
+const Footer: React.FC<FooterProps> = ({ title, description, socials }: FooterProps) => {
   const { allFile }: FooterData = useStaticQuery(graphql`
     query {
       allFile(
@@ -49,7 +48,10 @@ const Footer: React.FC<FooterProps> = ({ title, description, socials, menus }: F
                   <IconButton color="white" className="rounded-full">
                     <GatsbyImage
                       className="w-[45px]"
-                      image={allFile.edges[index].node.childImageSharp?.gatsbyImageData as IGatsbyImageData}
+                      image={
+                        allFile.edges[index].node.childImageSharp
+                          ?.gatsbyImageData as IGatsbyImageData
+                      }
                       alt={name}
                     />
                   </IconButton>
@@ -57,42 +59,12 @@ const Footer: React.FC<FooterProps> = ({ title, description, socials, menus }: F
               ))}
             </div>
           </div>
-          {/* <div className="mx-auto mt-8 grid w-max lg:grid-cols-2 lg:mt-0">
-            <div></div>
-            {menus.map(({ name, items }) => (
-              <div key={name}>
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  className="mb-2 block font-medium uppercase"
-                >
-                  {name}
-                </Typography>
-                <ul className="mt-3">
-                  {items.map((item) => (
-                    <li key={item.name}>
-                      <Typography
-                        as="a"
-                        href={item.path}
-                        target="_blank"
-                        rel="noreferrer"
-                        variant="small"
-                        className="mb-2 block font-normal text-blue-gray-500 hover:text-blue-gray-700"
-                      >
-                        {item.name}
-                      </Typography>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div> */}
         </div>
         <hr className="my-6 border-gray-300" />
         <div className="flex flex-wrap items-center justify-center md:justify-between">
           <div className="mx-auto w-full px-4 text-center">
             <Typography variant="small" className="font-normal text-blue-gray-500">
-              {`Copyright &copy; ${new Date().getFullYear()} ${title}`}
+              Copyright &copy; ${new Date().getFullYear()} ${title}
             </Typography>
           </div>
         </div>
