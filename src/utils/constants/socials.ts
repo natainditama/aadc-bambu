76430e10d3cf7b72a8ca23a2b2f1c309334686d1
natainditama <natainditama@gmail.com>
2023-01-15 19:55:14 +0800

    refactor: Add social links

---
+++ src/utils/constants/socials.ts
@@ -0,0 +1,25 @@
+interface Socials {
+  name: string;
+  icon: string;
+  url: string;
+}
+
+const socials: Array<Socials> = [
+  {
+    name: "instagram",
+    icon: "instagram.png",
+    url: "https://www.instagram.com/ayunibambu0404/",
+  },
+  {
+    name: "tiktok",
+    icon: "tiktok.png",
+    url: "https://www.tiktok.com/@aadcbambu",
+  },
+  {
+    name: "facebook",
+    icon: "facebook.png",
+    url: "https://www.facebook.com/igusti.murniati.1",
+  },
+];
+
+export { socials, Socials };
