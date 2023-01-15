f9cb965ae5decc5a452c3f231249d510349f7980
natainditama <natainditama@gmail.com>
2023-01-15 17:42:03 +0800

    feat: Destructured components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Layout from "./Layout/Layout";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import SEO from "./Seo/DefaultSeo";
+import VideoItem from "./VideoItem/VideoItem"; 
+
+export { Layout, Header, Footer, VideoItem, SEO };
