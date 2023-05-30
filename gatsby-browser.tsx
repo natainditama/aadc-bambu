42cde8a0b8ed2c0719523e47bf49179aff5833b4
Nata Inditama <natainditama@gmail.com>
2023-05-30 21:41:37 +0800

    styles: Import CSS dependencies

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,10 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "@fontsource/inter";
+import "aos/dist/aos.css";
+import "./src/styles/photofolio.css";
 import "./src/styles/global.css";
-import "@fontsource/lexend";
 import { Layout } from "./src/components";
 import * as React from "react";
 import type { GatsbyBrowser } from "gatsby";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min";
 
 
 export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({ element }) => {
