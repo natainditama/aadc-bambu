b1f7bb9d4786233ebaff72849c117a404d6b963a
Nata Inditama <natainditama@gmail.com>
2023-05-30 21:43:55 +0800

    feat: Revamp home page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,94 @@
 import React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import { Meta } from "@/components";
+import { useQuery } from "@tanstack/react-query";
+import { Video } from "@/types/videos";
+import useSiteMetadata from "@/utils/hooks/useSiteMetadata";
 
-const HomePage: React.FC<PageProps> = () => {
+export const Head: HeadFC = () => <Meta></Meta>;
+
+const CHANNEL_ID = "UCVVQiuzOR_MlSOax9KPnuIA";
+const API_KEY = process.env.GATSBY_YOUTUBE_API_KEY;
+const BASE_URL = process.env.GATSBY_YOUTUBE_API_URL;
+
+export default function HomePage() {
+  const { description } = useSiteMetadata()
+
+  const { status, data, error } = useQuery({
+    queryKey: ["videos"],
+    queryFn: async (): Promise<{ items: Array<Video> }> => {
+      const data = await fetch(
+        `${BASE_URL}search?part=snippet&part=id&channelId=${CHANNEL_ID}&maxResults=12&order=viewCount&type=video&videoDuration=medium&key=${API_KEY}`
+      );
+      return await data.json();
+    },
+  });
+
   return (
     <>
-      Hello
+      <section
+        id="hero"
+        className="hero d-flex flex-column justify-content-center align-items-center"
+        data-aos="fade"
+        data-aos-delay="1500"
+      >
+        <div className="container mt-5">
+          <div className="row justify-content-center">
+            <div className="col-lg-7 text-center">
+              <h1 className="fw-bold mb-3 lh-base">
+                Temukan <span>Konten Menarik</span> dan Nostalgia di YouTube AADC Bambu
+              </h1>
+              <p className="lh-base">{description}</p>
+              <a href="http://"></a>
+              <a
+                href={`https://www.youtube.com/channel/${CHANNEL_ID}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn-get-started"
+              >
+                Subscribe
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="gallery" className="gallery">
+        <div className="container">
+          {status === "loading" ? (
+            "Loading..."
+          ) : error instanceof Error ? (
+            <span>Error: {error.message}</span>
+          ) : (
+            <div className="row gy-4 justify-content-center">
+              {data?.items.map((item) => {
+                return (
+                  <div className="col-xl-4 col-lg-4 col-md-6" key={item.id.videoId}>
+                    <div className="gallery-item h-100">
+                      <img
+                        src={item.snippet.thumbnails.medium.url}
+                        alt={item.snippet.title}
+                        loading="lazy"
+                        className="img-fluid"
+                      />
+                      <div className="gallery-links d-flex align-items-center justify-content-center">
+                        <a
+                          href={`https://www.youtube.com/watch?v=${item.id.videoId}`}
+                          title={item.snippet.title}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="glightbox preview-link"
+                        >
+                          <i className="bi bi-arrows-angle-expand"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </section>
     </>
   );
-};
-
-export default HomePage;
+}
