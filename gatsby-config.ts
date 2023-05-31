c557f43923ad8633e33aaacf824ceb0feaf73a86
Nata Inditama <natainditama@gmail.com>
2023-06-01 02:18:31 +0800

    feat: Update google analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -42,7 +42,14 @@ const devPlugins = [
 
 const imagePlugins = ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"];
 
-const searchPlugins = ["gatsby-plugin-sitemap", "gatsby-plugin-robots-txt"];
+const searchPlugins = ["gatsby-plugin-sitemap", "gatsby-plugin-robots-txt"
+{
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [ process.env.GA_TRACKING_ID ]
+      }
+}
+];
 
 const pwaPlugins = [
   {
