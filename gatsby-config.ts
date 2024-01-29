7e5f914801bf29c8e2c7e9cb9c3a796e0be200bf
Nata Inditama <natainditama.dev@gmail.com>
2024-01-29 14:12:00 +0800

    chore: add path prefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,15 @@
 import type { GatsbyConfig } from "gatsby";
 import meta from "./gatsby-meta";
 import * as dotenv from "dotenv";
+import packageJson from "./package.json";
 
 dotenv.config({
   path: `.env.${process.env.NODE_ENV}`,
 });
+
+const isGitHubPages = process.env?.GITHUB_PAGES === "true";
+const isProduction = process.env?.NODE_ENV === "production";
+const pathPrefix = isGitHubPages && isProduction ? packageJson.name : "";
 
 const corePlugins = [
   {
@@ -35,7 +40,7 @@ const devPlugins = [
     resolve: "gatsby-plugin-alias-imports",
     options: {
       alias: {
-        "@/*": "src/",
+        "@/*": "src/*",
         "@/components": "src/components/",
         "@/hooks": "src/hooks/",
         "@/images": "src/images/",
@@ -84,7 +89,9 @@ const pwaPlugins = [
 ];
 
 const config: GatsbyConfig = {
+  assetPrefix: pathPrefix,
   siteMetadata: meta,
+  pathPrefix,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
